6fb132a60b8671799cf1ae9830b3702e74c02e39
BuildTools <unconfigured@null.spigotmc.org>
2021-02-01 21:53:23 -0500

    first commit

---
+++ src/main/kotlin/world/cepi/movement/ExampleExtension.kt
@@ -0,0 +1,15 @@
+package world.cepi.movement
+
+import net.minestom.server.movement.MovementExtension;
+
+class MovementExtension : Extension() {
+
+    override fun initialize() {
+        logger.info("[Movement-Abilities] has been enabled!")
+    }
+
+    override fun terminate() {
+        logger.info("[Movement-Abilities] has been disabled!")
+    }
+
+}
